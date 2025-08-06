0950b998255fb0e1d4b21d14297662b631f22444
Muhammad Andre Syahli <mandresyahliii16@gmail.com>
2025-08-06 11:49:20 +0700

    Menghapus Folder _components

--- resources/views/_components/navbar/mobile-link.blade.php
+++
@@ -1,4 +0,0 @@
-<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-{{-- aria-current="page" --}}
-<a {{ $attributes }}
-    class="{{ request()->fullUrlIs(url($href)) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium">{{ $slot }}</a>
